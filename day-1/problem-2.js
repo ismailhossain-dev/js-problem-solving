13ee03b18b78e4bda4683c805107a6ae7616d765
sabbir <programmarsabbir@gmail.com>
2026-06-15 17:23:35 +0800

    day-7 problem-3.js solve

--- day-1/problem-2.js
+++ day-1/problem-2.js
@@ -1,7 +1,7 @@
 // Problem 2: Check Even or Odd  [Easy]
 // Description: Write a function isEven(n) that returns true if a number is even, and false if it is odd.
 // Example:
-// Input: 4  → Output: trueInput: 7  → Output: false
+// Input: 4  → Output: true Input: 7  → Output: false
 // Hint: Use the modulus (%) operator.
 
 
--- day-7/problem-3.js
+++ day-7/problem-3.js
@@ -5,3 +5,46 @@ Example:
 const a = {x: {y: 1}};const b = deepClone(a);b.x.y = 99;// a.x.y is still 1
 Hint: Use recursion and check for object/array types.
  */
+
+// function deepClone(obj) {
+    
+// }
+
+// const a = 
+// {x: {y: 1}
+// }
+//man ta b te convert korbo 
+
+// const b = a;
+
+// console.log(b) //full object ta deka jaitese
+
+//upore object access korbo 
+//console.log(b.x.y);//output 1
+
+
+//============problem solve -33 ===============
+
+function deepClone(obj) {
+    if (obj === null || typeof obj !== "object") {
+        return obj;
+    }
+
+    const clone = Array.isArray(obj) ? [] : {};
+
+    for (let key in obj) {
+        clone[key] = deepClone(obj[key]);
+    }
+
+    return clone;
+}
+
+const a = {
+    x: {
+        y: 1
+    }
+};
+
+const b = deepClone(a);
+
+console.log(b);//output 1
