4e0ff6ac3c301f76ae7ce5edcf8a3331b192e7db
sabbir <programmarsabbir@gmail.com>
2026-06-21 23:19:29 +0800

    problem solving day1-problem1

--- day-1/problem-1.js
+++ day-1/problem-1.js
@@ -1,22 +1,18 @@
 // Problem 1: Swap Two Variables  [Easy]
 // Description: Write a function that swaps the values of two variables without using a third variable.
 // Example:
-// Input: a = 5, b = 10Output: a = 10, b = 5
+// Input: a = 5, b = 10 Output: a = 10, b = 5
 // Hint: Try using destructuring or arithmetic operators.
 
 //=====================================================
 
 //=============Problem-1 Solve==============
+//a er output 10 and b er ouput 5 korte hobe
 
-function swap(a, b) {
- a = a + b ; //5 + 10 =15
- b = a - b; //10-5 = 5 //output-1
-
- a= a - b ; //10-5 //output-5
-
- return {a , b}
-
+const swap = (a, b) => {
+    // এটাই মূল swap করার লাইন।
+    [a, b] = [b, a];
+    return {a, b}
 }
 
-console.log(swap(5, 10));
-
+console.log(swap(5, 10));//output 10 then 5
