eb1c4b19fa0570932efab750df2c2c1f8e69770c
sabbir <programmarsabbir@gmail.com>
2026-06-21 23:53:34 +0800

    day-1 problem solve-5

--- day-1/problem-5.js
+++ day-1/problem-5.js
@@ -1,24 +1,28 @@
 // Problem 5: Check Positive, Negative or Zero  [Easy]
 // Description: Write a function checkSign(n) that returns 'positive', 'negative', or 'zero' based on the value of n.
 // Example:
-// Input: -5  → Output: 'negative'Input: 0   → Output: 'zero'
+// Input: -5  → Output: 'negative'Input: 0   → Output: 'zero', input: 10 , Output Positive
 // Hint: Use if-else if-else statements.
 
 //===============Problem-5 solve=================
 
-const checkSign = (n)=> {
-    if(n === 0){
-        return "positive"
-    }
-    else if (n=== -5){
+const cheekSign = (n)=> {
+    //cheek n ki 0 teke choto kina
+    if(n < 0) {
         return "negative"
     }
-    else{
-        return "zero"
+    //cheek n teke 0 choto naki soman
+    else if(n <=0){
+        return "Zero"
+    }
+    //check n teke 10 boro naki soman
+    else if(n>=10){
+        return "positive"
     }
 }
 
-console.log(checkSign(0))
-console.log(checkSign(-5))
-console.log(checkSign(8))
+console.log(cheekSign(-5))//negative
+console.log(cheekSign(0))//Zero
+console.log(cheekSign(10))//positive
+
 
