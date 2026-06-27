d47985322fb14d054ba8cbc5343534710f2386e3
sabbir <programmarsabbir@gmail.com>
2026-06-27 23:09:49 +0800

    problem solve-9 ddone

--- day-2/problem9.js
+++ day-2/problem9.js
@@ -4,4 +4,40 @@ Example:
 Input: 'hello world'  → Output: 'Hello World'
 Hint: Use split(' '), map(), and join(' ')...
 */
-console.log("Hello");
+
+
+function titleCase(str) {
+    return str
+        .split(" ")
+        .map(word => word[0].toUpperCase() + word.slice(1))
+        .join(" ");
+}
+
+//console.log(titleCase("hello world"));
+
+
+/**
+ * slice(1) ব্যবহার করা হয় একটি string-এর নির্দিষ্ট index থেকে শেষ পর্যন্ত অংশ নেওয়ার জন্য। eta bisically inder er motho kaj kore first word ta dorche
+ * output
+ * what is slice 
+ * ello
+ * 
+ */
+
+const titleCase2 = (strr) => {
+    return strr
+        .split(" ")
+        .map((word) => word[0].toUpperCase() + word.slice(1))
+        .join(" ");
+}
+
+console.log(titleCase2("hello sabbir"));
+
+const titleCase3 = (strr) => {
+    return strr
+        .split(" ")
+        .map((word) => word[0].toUpperCase() + word.slice(1))
+        .join(" ");
+}
+
+console.log(titleCase3("hello sabbir"));//output Hello Sabbir
