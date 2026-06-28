1ef90a35237d14c6d06d1a7b75c43802e4939572
sabbir <programmarsabbir@gmail.com>
2026-06-28 20:05:52 +0800

    problem solve start

--- day-2/problem10.js
+++ day-2/problem10.js
@@ -2,5 +2,8 @@
 Description: Write a function countChar(str, char) that returns how many times a character appears in a string.
 Example:
 Input: 'banana', 'a'  → Output: 3
-Hint: Use split(char).length - 1 or a loop.
+Hint: Use split(char).length - 1 or a loop...
+
 */
+
+console.log("Hello");
--- day-3/problem-1.js
+++ day-3/problem-1.js
@@ -6,12 +6,3 @@ Hint: Use reduce() or a for loop.*/
 
 //======================problem solve 11======================
 
-//----The reduce() method is used to calculate the total value of all elements in an array. & use string convert to array
-
-//----------reduice take 2 argument-----------------
-
-const numbers = [1, 2, 3, 4, 5];
-
-const total = numbers.reduce((acc, curr) => acc + curr, 0);
-
-console.log(total);//output 15
