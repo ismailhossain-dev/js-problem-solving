a6867ad2d4ac9b6b79b0327b09d173166a76215e
sabbir <programmarsabbir@gmail.com>
2026-08-04 01:35:49 +0800

    solve js-problem2

--- day-1/problem-4.js
+++ day-1/problem-4.js
@@ -1,16 +1,27 @@
 // Problem 4: Celsius to Fahrenheit  [Easy]
 // Description: Write a function toFahrenheit(celsius) that converts a Celsius temperature to Fahrenheit.
 // Example:
-// Input: 0   → Output: 32Input: 100 → Output: 212
+// Input: 0   → Output: 32 Input: celsiuse → Output: 212
 // Hint: Formula: (C × 9/5) + 32
 
 
 //===============Problem-4 solve=================
 
+ const toFahrenheit = (celsius) => {
+    return (celsius  * 9/5) + 32;
+ }
 
-function toFahrenheit(celsius) {
-    return (celsius * 9 / 5) + 32;
-}
+ console.log(toFahrenheit(100))//output 212
+// (celsiuse × 9/5) + 32
 
-console.log(toFahrenheit(0));    // output 32
-console.log(toFahrenheit(100));  //output  212
+// = (celsiuse × 1.8) + 32
+
+// = 180 + 32
+
+// = 212
+
+console.log(toFahrenheit(0))
+
+// (0 × 9/5) + 32
+// = 0 + 32
+// = 32
