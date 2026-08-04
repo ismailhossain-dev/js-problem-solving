c8a5a1e7b0a32c0b1a32030ddf7cc22eaabedddd
sabbir <programmarsabbir@gmail.com>
2026-08-04 23:41:11 +0800

    start problem-solving

--- day-2/problem-3.js
+++ day-2/problem-3.js
@@ -7,23 +7,3 @@ Hint: Compare the string to its reverse.
 
 //============problem solve -8 ==========
 
-function isPalindrome(str) {
-
-  // string-টাকে character array-তে convert করা হচ্ছে
-  const reversed = str.split("")
-
-    // array-র elements উল্টে দেওয়া হচ্ছে
-    .reverse()
-
-    // আবার array থেকে string বানানো হচ্ছে
-    .join("");
-
-  // original string এবং reversed string একই কিনা compare করা হচ্ছে....
-  return str === reversed;
-}
-
-// "racecar" সামনে থেকে ও পিছন থেকে একই, তাই true return করবে
-console.log(isPalindrome("racecar")); // true
-
-// "hello" উল্টালে "olleh" হয়, তাই false return করবে
-console.log(isPalindrome("hello"));   // false
